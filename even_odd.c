bd84d605ff8540397b1974c4fc377d9140df6102
surajklmn <surajbhattarai339@gmail.com>
2023-02-24 17:31:28 +0545

    even_odd

---
+++ even_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int num;
+	printf("Enter a number: ");
+	scanf("%d",&num);
+	if(num%2 == 0){
+		printf("It is an even number. \n");
+
+	}
+	else{
+	printf("It is an odd number. \n");}
+	return 0;
+}
