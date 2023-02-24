38209c36dede2b428007225dc3856b04fbaafec5
surajklmn <surajbhattarai339@gmail.com>
2023-02-24 17:52:04 +0545

    vowelorconsonant

---
+++ vowel_consonant.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	char c;
+	printf("Enter a character: ");
+	scanf("%c",&c);
+	if(c == 'a'||c == 'A' || c == 'e' ||c=='E'|| c == 'i'|| c == 'I' || c == 'o' || c == 'O' || c == 'u' ||c == 'U'){
+		printf("Vowel :D\n");
+
+	}
+	else{
+		printf("Consonant \n");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
