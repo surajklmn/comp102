f1a8baf24ef226d0b0ac8611f65217f04dc13514
surajklmn <surajbhattarai339@gmail.com>
2023-02-25 17:32:21 +0545

    fixed algorithm

--- leap_year.c
+++ leap_year.c
@@ -3,23 +3,15 @@ int main(){
 	int a;
 	printf("Enter a year: ");
 	scanf("%i",&a);
-	if(a%4 == 0) {
-		if(a%100 == 0){
-			if(a%400 == 0){
-				printf("It is a leap year\n");
-
-			}
-			else{
-				printf("It is not a leap year \n");
-			}
-		}
-		else{
-			printf("It is not a leap year \n");
-		}
-		
-
-}
-else{
-	printf("It is not a leap year \n");
-}
+	if(a%4 == 0)
+		printf("It is a leap year.\n");
+	else if(a%100 == 0){
+		printf("It is not a leap year. \n");
+	}
+	else if(a%400 == 0){
+		printf("It is a leap year. \n");
+	}
+	else{
+	printf("It is not a leap year");
+	}
 }
