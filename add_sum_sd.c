93c103affd61e14fe5cdc44820d3aaadf3475382
Suraj Bhattarai <surajbhattarai339@gmail.com>
2023-03-12 22:43:37 +0545

    Holy Pointers

--- add_sum_sd.c
+++ add_sum_sd.c
@@ -1,31 +1,34 @@
-#include <stdio.h>
-int func(int a, int b, int c, int d, int e);
-int main(){
-	int s,t,u,v,w;
-	printf("Enter five integers :");
-	scanf("%i %i %i %i %i",&s,&t,&u,&v,&w);
-	printf("Sum is %i \n",func(s,t,u,v,w));
+#include<stdio.h>
+#include<math.h> //for sqrt function
+void func(int a, int b, int c, int d, int e, float *sum, float *avg);
 
+//main function
+void main()
+{
+	int a,b,c,d,e;
+	float sum, avg, std_dev;
 
+	printf("Enter 1st number: ");
+	scanf("%d", &a);
+	printf("Enter 2nd number: ");
+	scanf("%d", &b);
+	printf("Enter 3rd number: ");
+	scanf("%d", &c);
+	printf("Enter 4th number: ");
+	scanf("%d", &d);
+	printf("Enter 5th number: ");
+	scanf("%d", &e);
 
+	func(a,b,c,d,e,&sum,&avg);
 
-
-
-
-
-
-
-
-
-
-
-}
-int func(int a, int b, int c, int d, int e){
-	int s = a + b + c + d + e;
-	int *sum = &s;
-
-
-
+	printf("The Sum: %f\n", sum);
+	printf("The Average: %f\n", avg);
 
 }
 
+//func function
+void func(int a, int b, int c, int d, int e, float *sum, float *avg)
+{
+	*sum = a+b+c+d+e;
+	*avg = *sum/5.0;
+}
