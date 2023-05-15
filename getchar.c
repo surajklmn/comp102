e72edea61478b4e2c0a1ac390629bb215a3dbfa8
Suraj <surajbhattarai339@gmail.com>
2023-05-15 13:09:27 +0545

    getchar

---
+++ getchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	char answer;
+	printf("Are you gay?(y/n)\n");
+	answer = getchar();
+	if(answer == 'y' || answer == 'Y'){
+	printf("why are you gae?\n");
+	}else{
+	printf("Yes you is gae.\n");
+	}
+}
+
