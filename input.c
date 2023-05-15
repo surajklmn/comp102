9726bc9dc4e6ca63c1b89c948f10df04f1f53abf
Suraj <surajbhattarai339@gmail.com>
2023-05-15 14:16:59 +0545

    input

---
+++ input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,x,y,z;
+	int p,q,r;
+	printf("Enter three integer numbers\n");
+	scanf("%d %*d %d",&a,&b,&c);//* is used to skip the input
+	printf("%d %d %d\n",a,b,c);
+	printf("Enter two 4-digit numbers\n");
+	scanf("%2d %4d",&x,&y);
+	printf("%d %d \n \n",x,y);
+	printf("Enter two integers \n");
+	scanf("%d %d",&a,&x);
+	printf("%d %d\n \n",a,x);
+	printf("Enter a nine digit number \n");
+	scanf("%3d %4d %3d",&p,&q,&r);
+	printf("%d %d %d \n \n",p,q,r);
+	printf("Enter two three digit numbers \n");
+	scanf("%d %d",&x,&y);
+	printf("%d %d",x,y);
+}
