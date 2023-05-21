52cad8096db5eecd0323fd1dbd2e8a55dc365315
Suraj <surajbhattarai339@gmail.com>
2023-05-21 16:19:14 +0545

    fuckery

--- menu_driven_program.c
+++ menu_driven_program.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+unsigned long long factorial(int a);
+void prime(int a);
+void even(int a);
+
+
 int main(){
 	int num;
 	char choice;
@@ -14,13 +19,13 @@ int main(){
 	choice = toupper(choice);
 	switch(choice) {
 		case 'F':
-			printf("F to pay respect \n");
+			printf("Factorial of %d is %llu \n", num, factorial(num));
 		break;
 		case 'P':
-			printf("I see you have pressed P \n");
+		prime(num);
 		break;
 		case 'O':
-			printf("OOOOOOOOOOOOOOOOoooooooooooooo \n");
+			even(num);
 		break;
 
 		default:
@@ -28,5 +33,34 @@ int main(){
 		break;
 
 	}
+
+}
+unsigned long long factorial(int a){
+	long fact = 1;
+	for(int i = a; i > 0; i--){
+		fact = fact*i;
+	}
+	return fact;
+}
+
+void prime(int a){
+	if(a == 0 || a == 1){
+		printf("%d is not a prime number.",a);
+		}
+	else{
+		for(int i = 2; i <= a; i++){
+			if(a%i == 0 && i != a){
+				printf("It is not a prime number. \n");
+				break;
+				}
+			else if(i == a){
+				printf("It is a prime number. \n");
+			}
+		}
+	}	
+}
+
+void even(int a){
+	(a%2 == 0) ? printf("%d is even.\n",a): printf("%d is odd.\n",a);
 
 }
